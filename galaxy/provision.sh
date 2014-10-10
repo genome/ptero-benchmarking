796018872d5c28705c616684b8d0cc7f4abb4ed7
Mark Burnett <mark.m.burnett@gmail.com>
2014-10-10 09:12:54 -0500

    create galaxy user in correct database

--- galaxy/provision.sh
+++ galaxy/provision.sh
@@ -8,35 +8,37 @@ echo 'set -o vi' >> /root/.bashrc
 apt-get update
 apt-get install -y nginx postgresql postgresql-client vim-nox
 
-# Create galaxy user and database
+echo `date`: Create galaxy user and database 1>&2
 sudo -u postgres psql < /vagrant/postgres-setup.sql
 
 
-# Download galaxy software
+echo `date`: Downloading galaxy software 1>&2
 cd /tmp
 curl --silent --show-error -O https://bitbucket.org/galaxy/galaxy-dist/get/tip.tar.gz
 
 
-# Install galaxy software
+echo `date`: Install galaxy software 1>&2
 cd /home/vagrant
 sudo -u vagrant tar xf /tmp/tip.tar.gz
 sudo -u vagrant mv galaxy-galaxy-dist-* galaxy-dist
 
 
-# Install galaxy dependencies
+echo `date`: Installing galaxy dependencies 1>&2
 cd galaxy-dist
 sudo -u vagrant bash scripts/common_startup.sh
 
-# Install configuration
+echo `date`: Install configuration 1>&2
 cp -R /vagrant/config/* /
 
-
-rm /etc/nginx/sites-enabled/default
-
+echo `date`: Starting galaxy 1>&2
 service galaxy start
+
+echo `date`: Restarting nginx 1>&2
+rm /etc/nginx/sites-enabled/default
 service nginx restart
 
 
 # Wait for galaxy to create its database tables, then create a user
 sleep 300
-sudo -u postgres psql < /vagrant/galaxy-db-setup.sql
+echo `date`: Creating galaxy user 1>&2
+sudo -u postgres psql -d galaxy < /vagrant/galaxy-db-setup.sql
