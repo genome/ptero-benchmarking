e30b915e005c5a769b9eb8c7b4ead6793667898e
Mark Burnett <mark.m.burnett@gmail.com>
2014-10-10 08:35:12 -0500

    fix galaxy provisioning

--- galaxy/provision.sh
+++ galaxy/provision.sh
@@ -1,29 +1,42 @@
 #!/usr/bin/env bash
 
+set -e
+
+echo 'set -o vi' >> /home/vagrant/.bashrc
+echo 'set -o vi' >> /root/.bashrc
+
 apt-get update
 apt-get install -y nginx postgresql postgresql-client vim-nox
 
+# Create galaxy user and database
 sudo -u postgres psql < /vagrant/postgres-setup.sql
 
-if [ ! -f /tmp/tip.tar.gz ]; then
-    cd /tmp
-    curl --silent --show-error -O https://bitbucket.org/galaxy/galaxy-dist/get/tip.tar.gz
-fi
 
+# Download galaxy software
+cd /tmp
+curl --silent --show-error -O https://bitbucket.org/galaxy/galaxy-dist/get/tip.tar.gz
+
+
+# Install galaxy software
 cd /home/vagrant
-if [ ! -d galaxy-dist ]; then
-    sudo -u vagrant tar xf /tmp/tip.tar.gz
-    sudo -u vagrant mv galaxy-galaxy-dist-* galaxy-dist
-    cd galaxy-dist
-    sudo -u vagrant bash scripts/common_startup.sh
-    sudo -u vagrant python scripts/manage_db.py
-    cd ..
+sudo -u vagrant tar xf /tmp/tip.tar.gz
+sudo -u vagrant mv galaxy-galaxy-dist-* galaxy-dist
 
-    sudo -u postgres psql < /vagrant/galaxy-db-setup.sql
-fi
 
+# Install galaxy dependencies
+cd galaxy-dist
+sudo -u vagrant bash scripts/common_startup.sh
+
+# Install configuration
 cp -R /vagrant/config/* /
-rm /etc/nginx/sites-enables/default
+
+
+rm /etc/nginx/sites-enabled/default
 
 service galaxy start
 service nginx restart
+
+
+# Wait for galaxy to create its database tables, then create a user
+sleep 300
+sudo -u postgres psql < /vagrant/galaxy-db-setup.sql
